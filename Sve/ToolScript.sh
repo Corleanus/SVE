2eb0e7e5ba82267fe2dfbf291ec5cc3144026443
Corleanus <28986471+Corleanus@users.noreply.github.com>
2017-11-08 00:31:23 +0100

    Add files via upload

---
+++ Sve/ToolScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/killswitch-GUI/CobaltStrike-ToolKit.git
+
+git clone https://github.com/Und3rf10w/Aggressor-scripts.git
+
+git clone https://github.com/byt3bl33d3r/MITMf
+
+git clone https://github.com/Mi-Al/WiFi-autopwner
+
+git clone https://github.com/k4m4/kickthemout
+
+apt-get update
+
+apt-get purge openjdk-\*
+
+apt-get purge java-package
